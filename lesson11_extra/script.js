591ca485265e4e0d76c60c99d40bfa5bc32d18a1
Vasily <kuremkarmerurk@gmail.com>
2018-12-06 19:59:44 +0100

    Померать, так с музыкой!!!

--- lesson11_extra/script.js
+++ lesson11_extra/script.js
@@ -84,45 +84,14 @@ window.addEventListener('DOMContentLoaded', function(){
                             field.appendChild(cC.card);
                     }                    
                 });
-                
-                console.log(carFile.cars);
               }
         };          
      }
 
     loadJSON('от А до Z','init');
     go.addEventListener('click', function(){
-        //console.log(sort.options[sort.selectedIndex].text);
         loadJSON(sort.options[sort.selectedIndex].text,land.options[land.selectedIndex].text);
     });    
-
-    /*phone.addEventListener('input', function(){
-        let val = (phone.value.length < 4) ? '+49('.split('') : phone.value.split(''),
-            map = "+49(___)______-___".split('');
-            if( val.length < map.length ){
-                if( val[val.length-1].match(/[0-9]/) ){
-                    //map[val.length-1] == '_'
-                    if(map[val.length - 1] != '_'){
-                        val.splice(val.length - 1, 0 ,map[val.length - 1]);
-                    }
-                    phone.value = val.join('');
-                } else {                    
-                    if (val.length > 4){
-                        val.pop();
-                    }                    
-                    phone.value = val.join('');
-                }
-            } else {
-                val.pop();
-                phone.value = val.join('');
-            }
-    });
-
-    phone.addEventListener('focusout', function(){
-        if ( phone.value == "+49(" ){
-            phone.value = '';
-        }
-    });*/
 
 });
 
